ad34497854b312abcb9dcc6a6560da1b3ee8a91a
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2025-11-15 03:18:31 -0500

    Time: 11 ms (91.21%) | Memory: 18.5 MB (70.26%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,7 +1,11 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-      # Set a Target Index
+      # Set a Target Index (end of array)
+      # Then Go Backwards to See if It is Reachable From the Previous Index
       # If You Can Hit A Target Index that is the New Target Index
+      # Since if you can get to it you can get to the previous target
+      # The Aim is to Get to 0 meaning you can reach the final target from 0\
+      # Since it Can Reach Previous Targets that Can Reach the End 
 
         target = len(nums) - 1 
 
@@ -9,4 +13,5 @@ class Solution:
             if nums[i] >= (target - i):
                 target = i
 
+        # 0 returns False, Other integer return True so flip it to return the correct result + make it boolean
         return not (target)  
