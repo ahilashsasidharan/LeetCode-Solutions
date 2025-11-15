9692bf54667ed18720c7239f7682455e3563206f
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2025-11-14 20:27:13 -0500

    Time: 7 ms (72.93%) | Memory: 20.9 MB (65.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,37 +1,27 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        a = 0 
-        b = 1
-        while a < len(intervals) - 1:
-            # case 1: intervals a & b do not overlap
-            if intervals[a][1] < intervals[b][0] or intervals[b][1] < intervals[a][0]:
-                b += 1 
-            # case 2: a overlaps b where a starts before b
-            elif intervals[a][0] < intervals[b][0] and intervals[a][1] < intervals[b][1]:
-                newInt = [intervals[a][0], intervals[b][1]]
-                intervals[a] = newInt
-                intervals.pop(b) 
-                b = a + 1 # removing index b so do not increment
-            # case 3: b overlap a where b starts before a
-            elif intervals[b][0] < intervals[a][0] and intervals[b][1] < intervals[a][1]:
-                newInt = [intervals[b][0], intervals[a][1]]
-                intervals[a] = newInt
-                intervals.pop(b) 
-                b = a + 1 # removing index b so do not increment
-            # case 4: a engulfs b
-            elif intervals[a][0] <= intervals[b][0] and intervals[a][1] >= intervals[b][1]:
-                intervals.pop(b) 
-                # if a engulfs b there will be no new overlap for previously check intervals
-                b = b # removing index b so do not increment
-            # case 5 b engulfs a
+        mergedintervals = []
+
+        # Sort the list by first element simplifies the problem for the following reasons:
+        # 1. Removes the case where there is no overlap due to second interval being before the first since start
+        #    of the interval of the second will always be equal or larger then the first due to sorting
+        # 2. Makes it so if you discover no overlap for an interval then all future intervals will not have overlap
+        #    in the list since the start will be after the interval due to sorting
+        intervals.sort(key=lambda x: x[0])
+
+        prev = intervals[0]
+        curr = 1
+
+        while curr < len(intervals):
+            if intervals[curr][0] <= prev[1]:
+                prev = [prev[0], max(prev[1], intervals[curr][1])]
+                curr += 1
             else:
-                newInt = intervals[b]
-                intervals[a] = newInt
-                intervals.pop(b) 
-                b = a + 1 # removing index b so do not increment
+                mergedintervals.append(prev)
+                prev = intervals[curr]
+                curr += 1
 
-            if b >= len(intervals):
-                a += 1
-                b = a + 1
+        mergedintervals.append(prev)
 
-        return intervals
+        return mergedintervals
+
