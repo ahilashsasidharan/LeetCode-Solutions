53db3a55be258e0b846d01acbc357e86017ecd94
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2025-12-06 16:50:56 -0500

    Time: 0 ms (100.00%) | Memory: 20.4 MB (61.59%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -3,20 +3,22 @@ public:
     int jump(vector<int>& nums) {
         // Have a maxJmpRange to pick next jump within range of the step you are jumping from
         // (Works since you must always jump from the step 0)
+        // Edge Case Unless Size of Arr is 1 then You Are Already at n - 1 index
+        // So to Deal with This Start Algo/Loop at index = 1
         // Keep checking within maxJmpRange for which jump to choose
-        // Update maxJmpRange once the current one is exceeded using a msxReach calculated from steps you can take     
+        // Update maxJmpRange once the current one is exceeded using a reach calculated from steps you can take     
         // Note: don't need a reachable since it is assumed the final target is always reachable
         // Also need a count variable for steps taken and can stop once a step is reached that can hit n-1
 
         //  pair<int, int> step = (0, 0);
-        int reach = 0;
+        int maxReach = 0;
         int jumps = 0;
         int maxJmpRange = nums[0];
 
         for(int i = 1; i < nums.size(); i++){
             if(maxJmpRange < i){
-                maxJmpRange = reach;
-                reach = 0;
+                maxJmpRange = maxReach;
+                maxReach = 0;
                 jumps++;
             }
 
@@ -24,8 +26,8 @@ public:
                 return jumps + 1;
             }
 
-            if(nums[i] + i > reach){
-                reach = nums[i] + i;
+            if(nums[i] + i > maxReach){
+                maxReach = nums[i] + i;
             }
         }
 
