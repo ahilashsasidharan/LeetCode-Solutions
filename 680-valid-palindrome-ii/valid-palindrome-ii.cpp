eb2c5def7f03f18c8b9fbe5522640a306619991d
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2025-12-16 21:57:18 -0500

    Time: 4 ms (59.12%) | Memory: 26.7 MB (31.78%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -19,6 +19,7 @@ public:
         // check if the string is a palindrome
         // if at some point the string is not a palindrome check if moving the left or right pointer makes it one
         // if you can then continue otherwise fail
+        // Ignore this logic Incorrect:
         // do this once and use a boolean to keep track of this
         // if there is another point where it is not a palindrome fail
 
