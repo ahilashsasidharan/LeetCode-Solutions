798a8acf9171a198ae0eab75f6ad7a4511fc47a6
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2025-12-16 21:56:35 -0500

    Time: 12 ms (8.41%) | Memory: 26.6 MB (31.78%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isPalindrome(string s, int lp, int rp){
+        while(lp < rp){
+            if(s[lp] == s[rp]){
+                lp++;
+                rp--;
+            }
+            else{
+                return false; 
+            }
+        }
+
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        // Strategy:
+        // check if the string is a palindrome
+        // if at some point the string is not a palindrome check if moving the left or right pointer makes it one
+        // if you can then continue otherwise fail
+        // do this once and use a boolean to keep track of this
+        // if there is another point where it is not a palindrome fail
+
+        int lp = 0;
+        int rp = s.size() - 1; 
+
+        while(lp < rp){
+            if(s[lp] == s[rp]){
+                lp++;
+                rp--;
+            }
+            else{
+                return isPalindrome(s, lp + 1, rp) || isPalindrome(s, lp, rp - 1); 
+            }
+        }
+
+        return true;
+    }
+};
