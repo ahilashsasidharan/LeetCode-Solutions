5c5095f7ec66aecf950b0b9ab6977a2f95b95006
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2025-12-18 19:42:50 -0500

    Time: 0 ms (100.00%) | Memory: 10.3 MB (57.95%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
+    bool isVowel(char c){
+        // since s consist of printable ASCII characters this is safe
+        // but if not c should be cast to an unsigned_char to prevent undefined behaviour
+        // where c is not between [0, 255] i.e. -1 or not EOF
+        // because std::tolower requires EOF or an unsigned char value.
+        c = tolower(c);
+        return c == 'a' || c =='e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
     string reverseVowels(string s) {
+
         int lp = 0;
+
+        // safe to do in this case since s.length is guranteed to be >=1
+        // if it wasn't s.size() should be casted to an int before the subtraction
+        // or something else should be done to prevent underflow when s.size() equals 0 (i.e. s is empty)
         int rp = s.size() - 1;
-        set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
 
         while(lp < rp){
-            if(vowels.count(tolower(s[lp])) && vowels.count(tolower(s[rp]))){
-                char temp = s[lp];
-                s[lp] = s[rp];
-                s[rp] = temp;
-                lp++;
-                rp--;
-            }
-            else if(vowels.count(tolower(s[lp]))){
-                rp--;
-            }
-            else if(vowels.count(tolower(s[rp]))){
+            if(!isVowel(s[lp])){
                 lp++;
             }
+            else if(!isVowel(s[rp])){
+                rp--;
+            }   
             else{
+                swap(s[lp], s[rp]);
                 lp++;
                 rp--;
             }
