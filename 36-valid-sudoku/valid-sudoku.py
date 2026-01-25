5e0eb292d61adeed499e9a53968cee04bb97ea23
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2026-01-24 20:46:09 -0500

    Time: 12 ms (8.22%) | Memory: 19.4 MB (13.79%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -1,32 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        
-        # Check Rows
-        for row in board:
-            vals = set()
-            for cell in row:
-                if cell in vals and cell != ".":
-                    return False
-                vals.add(cell)
 
-        # Check Columns
-        for col in range(len(board[0])):
+        rows = {}
+        cols = {}
+        grids = {}
+
+        for i in range(9):
+            rows[i] = set()
+            cols[i] = set()
+            grids[i] = set()
+
+        for row in range(len(board)):
             vals = set()
-            for row in range(len(board)):
+            for col in range(len(board[0])):
+                grid = int(row / 3) + 3 * int(col / 3)
                 cell = board[row][col]
-                if cell in vals and cell != ".":
+                
+                if cell in rows[row] and cell != ".":
                     return False
-                vals.add(cell)
+                
+                if cell in cols[col] and cell != ".":
+                    return False
+                
+                if cell in grids[grid] and cell != ".":
+                    return False
 
-        # Check 3 x 3 Grids
-        for hgrid in range(0, int(len(board[0])), 3):
-            for vgrid in range(0, int((len(board))), 3):
-                vals = set()
-                for hind in range(hgrid, hgrid + 3):
-                    for vind in range(vgrid, vgrid + 3):
-                        cell = board[vind][hind]
-                        if cell in vals and cell != ".":
-                            return False
-                        vals.add(cell)
-                        
+                rows[row].add(cell)
+                cols[col].add(cell)
+                grids[grid].add(cell)
+
         return True
