d4e5abf71188040fe116da42761d41b3992c6012
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2026-01-31 15:10:35 -0500

    Time: 0 ms (100.00%) | Memory: 8.6 MB (49.39%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int currSquare = 0;
-        for(int i = 0; i <= x; i++){
-        // Cast i * i to long long guaranteed to be at least 64 bits.
-        // Prevents overflow when you do an operation that goes above the size of an integer
-        if(1LL * i * i > x){ 
-            break;
+        int currSquare {0};
+
+        // Return x for x < 2 (Binary search does not work for x = 1)
+
+        if(x < 2) return x; 
+        
+        // Perform Binary Search on the Potential Answer Space
+        // Which is inclusive between 0 and x/2 for x > 2
+
+        int left = 0;
+        int right = x / 2;
+        int middle = right / 2;
+
+        while(left <= right){
+            
+            // Cast middle * middle to long long guaranteed to be at least 64 bits.
+            // Prevents overflow when you do an operation that goes above the size of an integer
+            
+            if(1LL * middle * middle > x){ 
+                right = middle - 1;
+            }
+            else{
+                currSquare = middle;
+                left = middle + 1;
+            }
+
+            middle = (left + right) / 2;
+        
         }
-        currSquare = i;
-        }
+
         return currSquare;
     }
 };
