135a62e5456cfe84bd30b9a329dcf6b3ca69d493
ahilashsasidharan <79016853+ahilashsasidharan@users.noreply.github.com>
2026-07-25 22:27:44 -0400

    Time: 3 ms (37.04%) | Memory: 30.3 MB (91.69%) - LeetSync

---
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int LP = 0;
+        int RP = nums.size() - 1;
+
+        // to find the shortest unsorted continuous subarray
+        // you need to find from left the longest continous increasing subarray
+        // and from right the longest decreasing subarray
+        // then go through the array and step the pointer back
+        // once you for LP find a number smaller than it and RP bigger than it
+
+        while(LP < nums.size() - 1 && nums[LP] <= nums[LP + 1]){
+            LP++;
+        }
+
+        cout << LP << endl;
+
+        while(RP > 0 && nums[RP] >= nums[RP - 1]){
+            RP--;
+        }
+
+        cout << RP << endl;
+
+        if(LP >= RP) return 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            while(i > LP && LP >= 0 && nums[LP] > nums[i]){
+                LP--;
+            }
+
+            while(i < RP && RP < nums.size() && nums[RP] < nums[i]){
+                RP++;
+            }
+        }
+
+        cout << LP << endl;
+        cout << RP << endl;
+
+        return RP - LP - 1;
+    }
+};
